df889377abd533a7052bae5002585c4ad14412ad
stakeout5 <mike@stk5.com>
2014-02-24 09:17:47 -0500

    Initial Launch
    Basic calculator in VB

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnMult = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.frst = New System.Windows.Forms.Label()
+        Me.num2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(86, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(184, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(86, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(184, 22)
+        Me.TextBox2.TabIndex = 1
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(13, 118)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(106, 30)
+        Me.btnSubtract.TabIndex = 2
+        Me.btnSubtract.Text = "Subtract"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(167, 118)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(103, 30)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnMult
+        '
+        Me.btnMult.Location = New System.Drawing.Point(13, 189)
+        Me.btnMult.Name = "btnMult"
+        Me.btnMult.Size = New System.Drawing.Size(106, 30)
+        Me.btnMult.TabIndex = 4
+        Me.btnMult.Text = "Multiply"
+        Me.btnMult.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(167, 189)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(101, 30)
+        Me.btnDivide.TabIndex = 5
+        Me.btnDivide.Text = "Divide"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'frst
+        '
+        Me.frst.AutoSize = True
+        Me.frst.Location = New System.Drawing.Point(10, 15)
+        Me.frst.Name = "frst"
+        Me.frst.Size = New System.Drawing.Size(70, 17)
+        Me.frst.TabIndex = 6
+        Me.frst.Text = "Number 1"
+        '
+        'num2
+        '
+        Me.num2.AutoSize = True
+        Me.num2.Location = New System.Drawing.Point(13, 66)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(70, 17)
+        Me.num2.TabIndex = 7
+        Me.num2.Text = "Number 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.frst)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMult)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnSubtract As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnMult As System.Windows.Forms.Button
+    Friend WithEvents btnDivide As System.Windows.Forms.Button
+    Friend WithEvents frst As System.Windows.Forms.Label
+    Friend WithEvents num2 As System.Windows.Forms.Label
+
+End Class
---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+
+    Private Sub btnSubtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubtract.Click
+        Dim ans As Double = subtract(TextBox1.Text, TextBox2.Text)
+        MessageBox.Show(ans)
+    End Sub
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        Dim ans As Double = add(TextBox1.Text, TextBox2.Text)
+        MessageBox.Show(ans)
+    End Sub
+    Private Sub btnMult_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMult.Click
+        Dim ans As Double = multiply(TextBox1.Text, TextBox2.Text)
+        MessageBox.Show(ans)
+    End Sub
+    Private Sub btnDivide_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDivide.Click
+        Dim ans As Double = divide(TextBox1.Text, TextBox2.Text)
+        MessageBox.Show(ans)
+    End Sub
+
+    'subtract
+    Private Function subtract(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 - num2
+    End Function'end
+
+    'add
+    Private Function add(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 + num2
+    End Function
+    'multiply
+    Private Function multiply(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 * num2
+    End Function
+
+    'divide function
+    Private Function divide(ByVal num1 As Double, ByVal num2 As Double)
+        Return num1 / num2
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
